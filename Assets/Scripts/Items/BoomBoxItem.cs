85b12fdcb96ef72b4d514fde4f75ba1d2fb54d79
Joel Thomas <j.thomas@saedubai.com>
2019-09-02 01:44:41 +0400

    no message

---
+++ Assets/Scripts/Items/BoomBoxItem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Item that when used changes to the next song, when out of songs turns off, when used while off, plays first song.
+/// 
+/// TODO; It should auto play, randomise order potentially and go to next track when used.
+///     In other words, act kind of like the radio in a GTA style game.
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class BoomBoxItem : InteractiveItem
+{
+    //TODO: you will need more data than this, like clips to play and a way to know which clip is playing
+    protected AudioSource audioSource;
+    public AudioClip[] songs;
+    public bool isPlaying = true;
+    public int NowPlaying = 0;
+
+    protected override void Start()
+    {
+        base.Start();
+        audioSource = GetComponent<AudioSource>();
+        PlayClip();        
+    }
+
+    public void PlayClip()
+    {
+        audioSource.clip = songs[NowPlaying];
+        audioSource.Play();
+    }
+
+    public override void OnUse()
+    {
+        base.OnUse();
+
+        if (!isPlaying)
+        {
+            isPlaying = true;
+            NowPlaying = 0;
+            PlayClip();
+            return;
+        }
+
+        NowPlaying++;
+
+        if (NowPlaying >= songs.Length)
+        {
+            isPlaying = false;
+            audioSource.Stop();
+        }
+
+        else
+            PlayClip();
+    }
+}
---
+++ Assets/Scripts/Items/NerfGunItem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Item that when used while held acts as a physics based projectile instantiator
+/// </summary>
+public class NerfGunItem : InteractiveItem
+{
+    public GameObject nerfDartPrefab;
+    public Transform nerfDartSpawnLocation;
+    public float fireRate = 1;
+    public float launchForce = 10;
+    protected float fireRateCounter;
+
+    protected void Update()
+    {
+        fireRateCounter += Time.deltaTime;
+    }
+
+    public override void OnUse()
+    {
+        base.OnUse();
+
+        if (isHeld)
+        {
+            if (fireRateCounter > fireRate)
+            {
+                fireRateCounter = 0;
+                FireNow();
+            }
+        }
+    }
+        
+    public void FireNow()
+    {
+        GameObject NewDart = Instantiate(nerfDartPrefab, nerfDartSpawnLocation.position, nerfDartSpawnLocation.rotation);
+        Rigidbody NewDartRB = NewDart.GetComponent<Rigidbody>();
+
+        if (NewDartRB != null)
+        {
+            NewDartRB.AddRelativeForce(launchForce * Vector3.forward);
+        }  
+    }
+}
